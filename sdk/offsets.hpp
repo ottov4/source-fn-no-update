f1d2c3e32f74b36c606aacafcc983380faeaacbf
ottov4 <161163750+ottov4@users.noreply.github.com>
2024-09-23 18:22:15 +0100

    offsets.hpp

--- sdk/offsets.hpp
+++ sdk/offsets.hpp
@@ -1,27 +1,27 @@
 #pragma once
 #define OwningWorld 0xc0 - 0x20 //https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=ULevel
 #define levelssss 0x178 // this src define the offsets so if i do levels only it get errors bc there is imgui shit called levels so ykyykykykykyky https://dumpspace.spuckwaffel.com/Games/?hash=6b77eceb&type=classes&idx=UWorld
-#define GWORLD 0x118011A8
+#define GWORLD 0x1215C178 // update
 #define GAME_INSTANCE 0x1D8
 #define LOCAL_PLAYERS 0x38
 #define PLAYER_CONTROLLER 0x30
 #define LOCAL_PAWN 0x338
-#define CURRENT_WEAPON 0xA68
-#define VELOCITY 0x1CE0 
-#define GRAVITY 0x1CE4   
+#define CURRENT_WEAPON 0x9d8 // update
+#define VELOCITY 0x168 // update 
+#define GRAVITY 0x1E44 // maybe update but the old is 0x1CE4  
 #define PAWN_PRIVATE 0x308
 #define ROOT_COMPONENT 0x198
 #define RELATIVE_LOCATION 0x120
 #define PLAYER_STATE 0x2B0
 #define GAME_STATE 0x160
 #define PLAYER_ARRAY 0x2A8
-#define MESH 0x318
-#define COMPONENT_TO_WORLD 0x1c0
-#define BONE_ARRAY 0x5B8
-#define BONE_ARRAY_CACHE 0x600
-#define LAST_SUMBIT_TIME 0x2E8
-#define LAST_SUMBIT_TIME_ON_SCREEN 0x2F0
+#define MESH 0x310 // maybe update but the old is 0x318
+#define COMPONENT_TO_WORLD 0x1c0 // idk maybe no update
+#define BONE_ARRAY 0x570 // update 
+#define BONE_ARRAY_CACHE 0x5B8 // update 
+#define LAST_SUMBIT_TIME 0x2E8 // no update
+#define LAST_SUMBIT_TIME_ON_SCREEN 0x2E0 // update
 #define PLATFORM 0x438
-#define HABANERO_COMPONENT 0x9F0 // ranked only
+#define HABANERO_COMPONENT 0x9E8 // ranked only (update)
 #define RANKED_PROGRESS 0xB8 // ranked only
 #define PERSISTENT_LEVEL 0x30
